ee61c6b37a691e323c133b12fc209b850d03f697
levinat <nat.levinzon@gmail.com>
2022-01-11 16:48:03 +0200

    Add project files.

---
+++ Shop.Core/ServiceInterface/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using Shop.Core.Domain;
+using Shop.Core.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.Core.ServiceInterface
+{
+    public interface IProductService : IApplicationService
+    {
+        Task<Product> Add(ProductDto dto);
+
+        Task<Product> Delete(Guid id);
+
+        Task<Product> Update(ProductDto dto);
+
+        Task<Product> GetAsync(Guid id);
+
+        //Task<ExistingFilePath> RemoveImage(ExistingFilePathDto dto);
+    }
+}
---
+++ Shop.Data/ShopDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Shop.Core.Domain;
+
+namespace Shop.Data
+{
+    public class ShopDbContext : IdentityDbContext
+    {
+        public ShopDbContext(DbContextOptions<ShopDbContext> options)
+            : base(options) { }
+
+        public DbSet<Product> Product { get; set; }
+
+        public DbSet<ExistingFilePath> ExistingFilePath { get; set; }
+        //kuidas yhendada aplikatsioon DB-ga
+    }
+}
